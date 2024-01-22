36a4b84e532d8ccab36e4a57cc7500e758231fe9
Monday_David_Sarka <davidsarka242@gmail.com>
2024-01-22 18:35:49 +0100

    Added readme file and router

---
+++ README.md
@@ -0,0 +1,7 @@
+### Express lambda Function on netlify
+
+
+### Installed:
+  *express*
+  **netlify-lambda**
+  *serverless-http*
--- src/api.js
+++ src/api.js
@@ -3,4 +3,16 @@ const serverless =  require("serverless-http");
 
 const app = express();
 
-module.exports.handler = serverless(app);
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.json({
+    "Hi:": "Hello"
+  })
+})
+
+app.use('./netlify/functions/api', router)
+
+
+
+module.exports.handlerssdsde = serverless(app);
